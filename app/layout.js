e21fe3f51039654e09a458e4741f6b4be3296885
tapishkatiyar01-dev <tapishkatiyar01@gmail.com>
2026-09-25 19:10:22 +0530

    issue:db loading while static page generation

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,8 @@ export const viewport = {
   viewportFit: 'cover',
 };
 
+export const dynamic = 'force-dynamic';
+
 export async function generateMetadata() {
   const data = await getPortfolioData();
   return buildPortfolioMetadata(data);
